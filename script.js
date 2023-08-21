373971a03d8a21bd5e96d24a56309b4323de29ec
Agus Setiawan <agus.lupin@gmail.com>
2023-08-21 16:59:17 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+var aceeditor = ace.edit('editor');
+aceeditor.setOptions({
+  wrap: true,
+  fontSize: 18,
+  tabSize: 2,
+  // minLines:23, maxLines:23,
+  autoScrollEditorIntoView: true,
+  highlightActiveLine: false,
+  vScrollBarAlwaysVisible: true,
+});
+aceeditor.setTheme('ace/theme/cobalt'); //  monokai dracula cobalt iplastic terminal
+aceeditor.session.setValue('Halllo world\n'.repeat(25));
+//aceeditor.focus();
+//aceeditor.session.setMode('ace/mode/javascript');
+
+$('#editorfind').click(function () {
+  aceeditor.execCommand('find');
+  //editor.searchBox.show()
+});
+$('#editorreplace').click(function () {
+  aceeditor.execCommand('replace');
+});
+
+$('#editor').css({
+  height: '350px',
+  resize: 'both',
+  overflow: 'hidden',
+  border: 'solid 0px lightgray',
+});
